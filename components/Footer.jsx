c974aef59eeec0b7414bf0d49e6dc82893595bc7
Dat Nguyen <dxn003@ucsd.edu>
2023-05-14 02:07:18 -0700

    Add components definitions

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AiFillFacebook } from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Celine's Desserts All rights reserved</p>
+      <p className="icons">
+        <AiFillFacebook />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Product, FooterBanner, HeroBanner, Footer } from '../components'
+import { client } from '../client'
+
+const Home = ( { products, bannerData }) => (
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>A variety of snacks and desserts</p>
+      </div>
+
+      <div className="products-container">
+        {products?.map((product) => <Product key={product._id} product={product} />)}
+      </div>
+
+      <FooterBanner />
+    </div>
+)
+
+export default Home
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
