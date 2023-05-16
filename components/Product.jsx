ae517cebf34f19ab6a208ccc2d03696a55cacd8d
Dat Nguyen <binhut123@gmail.com[A>
2023-05-16 13:29:30 -0700

    Edit feature

--- components/Product.jsx
+++ components/Product.jsx
@@ -8,9 +8,9 @@ const Product = ({ product: { image, name, slug, price } }) => {
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img src={urlFor(image && image[0])} width={350} height={350} className="prodcut-image" />
+          <img src={urlFor(image)} width={350} height={350} className="prodcut-image" />
           <p className="product-name">{name}</p>
-          <p className="product-price">${price}</p>
+          <p className="product-price">{price}</p>
         </div>
       </Link>
     </div>
--- sanity-backend/schemas/product.js
+++ sanity-backend/schemas/product.js
@@ -6,10 +6,9 @@ export default {
        {
            name: 'image',
            title: 'Image',
-           type: 'array',
-           of: [{ type: 'image'}],
+           type: 'image',
            options: {
-               hotspot: true,
+            hotspot: true
            } 
        },
        {
@@ -29,7 +28,7 @@ export default {
        {
            name: 'price',
            title: 'Price',
-           type: 'number',
+           type: 'string',
        },
        {
            name: 'details',
